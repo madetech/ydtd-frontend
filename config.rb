c6b39011c5dfcade5e0d3ed76c1629434f57b5a6
Luke Morton <luke@madebymade.co.uk>
2014-03-21 14:16:56 +0000

    Reorder middleman config.rb

--- config.rb
+++ config.rb
@@ -3,20 +3,10 @@ require 'bundler/setup'
 
 require 'modular-scale'
 require 'middleman-livereload'
+require 'middleman-minify-html'
 require 'app/helpers/ydtd_frontend/mailer_helper'
 
 helpers YdtdFrontend::MailerHelper
-
-after_configuration do
-  sprockets.append_path "#{root}/vendor/assets/javascripts"
-  sprockets.append_path "#{root}/vendor/assets/stylesheets"
-  sprockets.import_asset "modernizr.js"
-  sprockets.import_asset "jquery.min.js"
-end
-
-compass_config do |config|
-  config.output_style = :compact
-end
 
 set :source, "app"
 set :css_dir, "assets/stylesheets"
@@ -24,22 +14,32 @@ set :js_dir, "assets/javascripts"
 set :images_dir, "assets/images"
 set :fonts_dir, "assets/fonts"
 set :layouts_dir, 'views/layouts'
+set :layout_dir, 'views/layouts'
 
-page "/", :layout => "application"
-page "/tabbed_lists.html", :layout => "application"
-page "/tabbed_lists_2.html", :layout => "application"
-page "/profile.html", :layout => "application"
-page "/email.html", :layout => "mail"
-
+page "/", :layout => :application
+page "/tabbed_lists.html", :layout => :application
+page "/tabbed_lists_2.html", :layout => :application
+page "/profile.html", :layout => :application
+page "/email.html", :layout => :mail
 page "/marketing/"
 
 activate :livereload
 activate :i18n
 
 configure :build do
-  set :js_compressor, Uglifier.new(:toplevel => true, :unsafe => true)
   activate :asset_hash
   activate :minify_css
   activate :minify_javascript
   activate :minify_html
+end
+
+compass_config do |config|
+  config.output_style = :compact
+end
+
+after_configuration do
+  sprockets.append_path "#{root}/vendor/assets/javascripts"
+  sprockets.append_path "#{root}/vendor/assets/stylesheets"
+  sprockets.import_asset "modernizr.js"
+  sprockets.import_asset "jquery.min.js"
 end
